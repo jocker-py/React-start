b8f49bc3c9d98ef5d81aa6747a8561857bc1cd2f
jocker-py <buladzislau@gmail.com>
2022-10-22 19:26:00 +0300

    feat: add ref and disable properties for button

--- src/components/button/myButton.tsx
+++ src/components/button/myButton.tsx
@@ -1,12 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, LegacyRef } from 'react';
 import cl from './myButtion.module.css';
 
 interface IMyButtonProps {
   children: React.ReactNode;
+  link?: LegacyRef<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
-const MyButton: FC<IMyButtonProps> = ({ children }: IMyButtonProps) => {
-  return <button className={cl.myButton}>{children}</button>;
+const MyButton: FC<IMyButtonProps> = ({ children, link, disabled }: IMyButtonProps) => {
+  return (
+    <button className={cl.myButton} ref={link ? link : null} disabled={disabled ? true : undefined}>
+      {children}
+    </button>
+  );
 };
 
 export default MyButton;
