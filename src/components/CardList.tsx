cbddb43fa6501c53276afb6e6789639319ad06d8
jocker-py <buladzislau@gmail.com>
2022-10-22 19:49:45 +0300

    refactor: change class CardList to function

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,34 +1,18 @@
-import React from 'react';
-import CardItem from './CardItem';
+import React, { FC } from 'react';
 import cl from './CardList.module.css';
-import axios from 'axios';
-import { ICardItemProps, ICardListProps, ICardListState } from '../config/interfaces';
+import { ICardItemProps, ICardListProps } from '../config/interfaces';
+import CardItem from './Card/CardItem';
 
-class CardList extends React.Component<ICardListProps, ICardListState> {
-  constructor(props: ICardListProps) {
-    super(props);
-    this.state = { cards: [] };
-    this.fetchCards();
-  }
+const CardList: FC<ICardListProps> = (props) => {
+  return props.cards && props.cards.length ? (
+    <ul className={cl.card__list} data-testid="card-list">
+      {props.cards.map((card: ICardItemProps) => (
+        <CardItem key={card.id} card={card} />
+      ))}
+    </ul>
+  ) : (
+    <h1 className={cl.title}>Products not found</h1>
+  );
+};
 
-  fetchCards(): void {
-    async function fetchData(): Promise<ICardItemProps[]> {
-      const response = await axios.get<ICardItemProps[]>('https://fakestoreapi.com/products');
-      return response.data;
-    }
-    fetchData().then((res) => this.setState({ cards: res }));
-  }
-
-  createCards() {
-    return this.state.cards.map((card: ICardItemProps) => <CardItem key={card.id} card={card} />);
-  }
-
-  render() {
-    return (
-      <ul className={cl.card__list} data-testid="card-list">
-        {this.createCards()}
-      </ul>
-    );
-  }
-}
 export default CardList;
