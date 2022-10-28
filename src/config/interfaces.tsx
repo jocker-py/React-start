7d605575674d68acb92ede7c99aef02fd7776df8
jocker-py <buladzislau@gmail.com>
2022-10-28 17:34:39 +0300

    feat: add interfaces

--- src/config/interfaces.tsx
+++ src/config/interfaces.tsx
@@ -27,8 +27,73 @@ export interface ICardStars {
 export interface ISearchBarProps {
   placeholder?: string;
   value?: string;
+  title?: string;
+  setValue: (value: string) => void;
+  setPage: (page: number) => void;
 }
 
-export interface ISearchBarState {
-  readonly value: string;
+export interface ICharacterResponse {
+  info: {
+    count: number;
+    next: string | null;
+    pages: number;
+    prev: null | string;
+  };
+  results: ICharacterItem[];
+}
+
+export interface ICharacterItem {
+  created: string;
+  episode: string[];
+  gender: string;
+  id: number;
+  image: string;
+  location: { name: string; url: string };
+  name: string;
+  origin: { name: string; url: string };
+  species: string;
+  status: string;
+  type: string;
+  url: string;
+}
+
+export interface CharactersItemProps {
+  created: string;
+  episode: string[];
+  gender: string;
+  id: number;
+  image: string;
+  location: { name: string; url: string };
+  name: string;
+  origin: { name: string; url: string };
+  species: string;
+  status: string;
+  type: string;
+  url: string;
+}
+
+export interface CharactersListProps {
+  cards: CharactersItemProps[];
+  isVisible: boolean;
+  setIsVisible: (props: boolean) => void;
+  setCurrentId: (props: number) => void;
+}
+
+export interface CharactersItemProps {
+  created: string;
+  episode: string[];
+  gender: string;
+  id: number;
+  image: string;
+  location: { name: string; url: string };
+  name: string;
+  origin: { name: string; url: string };
+  species: string;
+  status: string;
+  type: string;
+  url: string;
+}
+
+export interface CharactersItem {
+  card: CharactersItemProps | null;
 }
