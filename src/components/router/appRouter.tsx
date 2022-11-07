5ba3d4464abcb5ad2356d531fe1e72c04f16aadc
jocker-py <buladzislau@gmail.com>
2022-11-07 16:29:15 +0300

    feat: add state and dispatch as props for each route

--- src/components/router/appRouter.tsx
+++ src/components/router/appRouter.tsx
@@ -5,16 +5,20 @@ import NotFound from '../../pages/NotFound';
 import Products from '../../pages/Products';
 import Home from '../../pages/Home';
 import Characters from '../../pages/Characters';
+import { IAppProps } from '../../redux/interfaces';
 
-const AppRouter: FC = () => {
+const AppRouter: FC<IAppProps> = ({ state, dispatch }) => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/products/:id" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/characters" element={<Characters />} />
-      <Route path="/*" element={<NotFound />} />
+      <Route path="/" element={<Home state={state.homePage} />} />
+      <Route path="/products" element={<Products state={state.productsPage} />} />
+      <Route path="/products/:id" element={<Home state={state.homePage} />} />
+      <Route path="/about" element={<About state={state.aboutPage} />} />
+      <Route
+        path="/characters"
+        element={<Characters state={state.charactersPage} dispatch={dispatch} />}
+      />
+      <Route path="/*" element={<NotFound state={state.notFoundPage} />} />
     </Routes>
   );
 };
