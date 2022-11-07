bc8337942182692d8de6b7a5eeee1af4c451c21b
jocker-py <buladzislau@gmail.com>
2022-11-07 16:36:10 +0300

    refactor: refactor component using custom redux

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -3,36 +3,16 @@ import SearchBar from '../searchBar/searchBar';
 import cl from './sideBar.module.css';
 import MySelect from '../select/MySelect';
 import MyButton from '../button/myButton';
+import { ISideBarProps } from '../../redux/interfaces';
 
-interface ISideBarProps {
-  name: string;
-  setName: (value: string) => void;
-  setPage: (page: number) => void;
-  status: string | null;
-  setStatus: (status: string) => void;
-  gender: string | null;
-  setGender: (gender: string) => void;
-  resetForm: () => void;
-}
-
-const SideBar: FC<ISideBarProps> = ({
-  name,
-  setName,
-  setPage,
-  status,
-  setStatus,
-  gender,
-  setGender,
-  resetForm,
-}) => {
-  const statusOptions = ['alive', 'dead', 'unknown'];
-  const genderOptions = ['male', 'female', 'unknown', 'genderless'];
+const SideBar: FC<ISideBarProps> = ({ state, dispatch }) => {
+  const { name, status, gender } = state;
   return (
     <div className={cl.sideBar}>
-      <SearchBar title="Name" value={name} setValue={setName} setPage={setPage} />
-      <MySelect title="Status" options={statusOptions} value={status} setValue={setStatus} />
-      <MySelect title="Gender" options={genderOptions} value={gender} setValue={setGender} />
-      <MyButton onClick={resetForm}>Reset</MyButton>
+      <SearchBar state={name} dispatch={dispatch} />
+      <MySelect state={status} dispatch={dispatch} />
+      <MySelect state={gender} dispatch={dispatch} />
+      <MyButton onClick={() => dispatch({ type: 'resetForm' })}>Reset</MyButton>
     </div>
   );
 };
