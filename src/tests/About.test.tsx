2323af8df845063288664aae5c2f57e552c0c150
jocker-py <buladzislau@gmail.com>
2022-11-07 16:22:22 +0300

    remove: remove testsv

--- src/tests/About.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from '../pages/About';
-import React from 'react';
-
-test('render About page', () => {
-  render(<About />);
-  const title = screen.getByText(/About Us/i);
-  expect(title).toBeInTheDocument();
-});
