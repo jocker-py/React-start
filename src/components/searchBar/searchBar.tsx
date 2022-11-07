08b22d1164376e99385fed200c30bee85bd57d24
jocker-py <buladzislau@gmail.com>
2022-11-07 16:33:16 +0300

    refactor: rewrite component using redux

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,26 +1,29 @@
 import React, { FC } from 'react';
 import cl from './searchBar.module.css';
-import { ISearchBarProps } from '../../config/interfaces';
+import { Dispatcher, DispatcherTypes } from '../../redux/interfaces';
 
-const SearchBar: FC<ISearchBarProps> = ({ title, value, setValue, setPage }) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setValue(e.target.value);
-    setPage(1);
-  };
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    setValue('');
-    setPage(1);
-  };
+export interface ISearchBarProps {
+  state: ISearchBarState;
+  dispatch: Dispatcher;
+}
+
+export interface ISearchBarState {
+  title: string;
+  value: string;
+  type: DispatcherTypes;
+}
+
+const SearchBar: FC<ISearchBarProps> = ({ state, dispatch }) => {
+  const { title, value, type } = state;
   return (
-    <form className={cl.form} onSubmit={handleSubmit} data-testid="form">
+    <form className={cl.form} onSubmit={() => dispatch({ type: 'resetForm' })}>
       <h5 className={cl.form__title}>{title ? title : ''}</h5>
       <input
         type="search"
         data-testid="search"
         value={value}
         placeholder="Search..."
-        onChange={handleChange}
+        onChange={(e) => dispatch({ type, value: e.target.value })}
         className={cl.form__input}
       />
     </form>
