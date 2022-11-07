dbec22f81cabe989768affb855a71d91dfaf0dda
jocker-py <buladzislau@gmail.com>
2022-11-07 16:38:24 +0300

    feat: add notice if page clear

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -2,6 +2,12 @@ import React, { FC } from 'react';
 import cl from './CharactersList.module.css';
 import { CharactersListProps } from '../config/interfaces';
 
+const titleStyles: React.CSSProperties = {
+  textAlign: 'center',
+  fontFamily: 'American Typewriter',
+  letterSpacing: '0.05cm',
+};
+
 const CharactersList: FC<CharactersListProps> = ({
   cards,
   isVisible,
@@ -14,15 +20,19 @@ const CharactersList: FC<CharactersListProps> = ({
   };
   return (
     <div className={cl.card__list}>
-      {cards.map((card) => (
-        <div className={cl.card__item} key={card.id}>
-          <div className={cl.card__link} onClick={() => showModal(card.id)}>
-            <div>look more</div>
+      {cards.length ? (
+        cards.map((card) => (
+          <div className={cl.card__item} key={card.id}>
+            <div className={cl.card__link} onClick={() => showModal(card.id)}>
+              <div>look more</div>
+            </div>
+            <div style={{ backgroundImage: `url(${card.image})` }} className={cl.card__image} />
+            <p className={cl.card__name}>{card.name}</p>
           </div>
-          <div style={{ backgroundImage: `url(${card.image})` }} className={cl.card__image} />
-          <p className={cl.card__name}>{card.name}</p>
-        </div>
-      ))}
+        ))
+      ) : (
+        <h3 style={titleStyles}>Characters not found</h3>
+      )}
     </div>
   );
 };
